f3e83f24e8dc8c3694636f36e5680699dd855ce3
AnshKhurana <anshkhurana1507@gmail.com>
2018-12-20 03:10:11 +0530

    fixed bug in dataset evaluation

---
+++ dataset_evaluation/histograms.py
@@ -0,0 +1,11 @@
+import json
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+def saturation_histo():
+    pass
+
+def value_histo():
+    pass
+
--- dataset_evaluation/test.py
+++ dataset_evaluation/test.py
@@ -4,18 +4,20 @@ import cv2
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    with open("../data/malaria/malaria/training.json", 'rU') as fIn:
+    with open("../data/malaria/malaria/test.json", 'rU') as fIn:
         data = json.load(fIn)
-    # categories = ['red blood cell', 'trophozoite', 'ring', 'schizont', 'difficult', 'gametocyte']
+    categories = ['red blood cell', 'trophozoite', 'ring', 'schizont', 'difficult', 'gametocyte']
     with open('classes.json', 'r') as fIn:
         categories = json.load(fIn)
     print(categories)
     image = data[0]
     image_name = image['image']['pathname']
     print("Working on image " + image_name)
-    im_path = '../data/malaria/malaria' + image_name
+    im_path = '../data/malaria/malaria'  + image_name
     print(im_path)
     im_mat = cv2.imread(im_path)
+    plt.imshow(im_mat)
+    plt.show()
     for object in image['objects']:
         bb = object['bounding_box']
         bb_min = bb['minimum']
@@ -26,17 +28,16 @@ if __name__ == "__main__":
         c_max = bb_max['c']
         cat = object['category']
         if cat == 'red blood cell':
-            cv2.rectangle(im_mat, (c_min, r_min), (c_max, r_max), (255, 0, 0), 3)
+            cv2.rectangle(im_mat, (r_min, c_min), (r_max, c_max), (255, 0, 0), 3)
         elif cat == 'trophozite':
-            cv2.rectangle(im_mat, (c_min, r_min), (c_max, r_max), (0, 0, 255), 3)
+            pass
         elif cat == 'ring':
-            cv2.rectangle(im_mat, (c_min, r_min), (c_max, r_max), (255, 255, 0), 3)
+            pass
         elif cat == 'schizont':
-            cv2.rectangle(im_mat, (c_min, r_min), (c_max, r_max), (255, 128, 0), 3)
+            pass
         elif cat == 'difficult':
-            cv2.rectangle(im_mat, (c_min, r_min), (c_max, r_max), (0, 0, 0), 3)
+            pass
         else:
-            cv2.rectangle(im_mat, (c_min, r_min), (c_max, r_max), (128, 64, 128), 3)
+            pass
     plt.imshow(im_mat)
     plt.show()
-    plt.imsave('sample_image', im_mat)
